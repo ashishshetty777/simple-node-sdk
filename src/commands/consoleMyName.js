a56a4dd398c8027920167a2ddb279e4fb30b331d
iotric <iotric@192.168.1.11>
2022-03-16 16:07:37 +0530

    Iteration 1 done

--- src/commands/consoleMyName.js
+++ src/commands/consoleMyName.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { baseUrl } from '../../constants';
+import { baseUrl } from '../constants';
 import {validateApiKeys} from '../util/validators';
 import { handleError } from '../util/errorResponse';
 
